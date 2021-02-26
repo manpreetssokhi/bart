62b0148cca7c05d5d90806025d03c1410da7e6b3
Manpreet Sokhi <manpreetsokhi99@gmail.com>
2021-02-26 01:43:43 -0800

    finished the three apis

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request = require('request');
 const http = require('http');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-const bartAPIKey = "ZR2V-5S6P-9W4T-DWEI";
+const bartAPIKey = "MW9S-E7SL-26DU-VV8V";
 
 const PORT = process.env.PORT || 4200;
 
@@ -31,18 +31,84 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/home', (req, res) => {
+// function for creating XML from JSON
+function getJSON(uri, callabck) {
+    http.get(uri, function(APIres) {
+        console.log('URI: ' + uri);
+        console.log('STATUS: ' + APIres.statusCode);
+        let bodyChunks = [];
+        APIres.on('data', function(chunk) {
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            let body = Buffer.concat(bodyChunks);
+            body = JSON.parse(body);
+            callabck(body);
+        })
+    });
+}
+
+app.get('/', (req, res) => {
     // res.send('hello bobby');
     let context = {};
-    res.render('home', context);
+    res.render('stationsList', context);
     console.log('in home');
 });
 
-app.get('/arrivals', (req, res) => {
+app.get('/stations', (req, res) => {
     // res.send('hello bobby');
     let context = {};
-    res.render('arrivals', context);
-    console.log('in arrivals');
+    
+    getJSON('http://api.bart.gov/api/stn.aspx?cmd=stns&key=' + bartAPIKey + '&json=y', function(body) {
+        // res.send(body.root.stations.station);
+        // console.log(body.root.stations.station);
+        context = body.root.stations.station;
+        console.log(context);
+        res.render('stations', context);
+    })
+
+    console.log('in stations');
+});
+
+app.get('/station', (req, res) => {
+    // res.send('hello bobby');
+    let context = {};
+    let station = req.query.station;
+
+    if (station !== undefined && station.length == 4) {
+        getJSON('http://api.bart.gov/api/stn.aspx?cmd=stninfo&key=' + bartAPIKey + '&orig=' + station + '&json=y', function(body) {
+            context = body.root.stations.station;
+            console.log(context);
+            res.render('station', context);
+        })
+    } else {
+        res.status(400).send({
+            message: 'no station was sent by BART API'
+        });
+    }
+
+    res.render('station', context);
+    console.log('in station');
+});
+
+app.get('/trips', (req, res) => {
+    // res.send('hello bobby');
+    let context = {};
+    let source = req.query.source;
+    let destination = req.query.destination;
+
+    if (source !== undefined && destination !== undefined) {
+        getJSON('http://api.bart.gov/api/sched.aspx?cmd=depart&key=' + bartAPIKey + '&orig=' + source + '&dest=' + destination + '&date=now&b=0&a=4&l=1&json=y', function(body) {
+            context = body.root.stations.station;
+            console.log(context);
+            res.render('trips');
+        })
+    } else {
+        res.status(400).send({
+            message: 'no station was sent by BART API'
+        });
+    }
+    res.render('trips', context);
+    console.log('in trips');
 });
 
 app.listen(PORT, function () {
