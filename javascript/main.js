464098509e7552859a32fead617d86ba0e83b828
Manpreet Sokhi <manpreetsokhi99@gmail.com>
2021-03-01 01:33:25 -0800

    maps works - need to work on transit layer and api for lat long

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,12 @@ $(document).ready(function () {
     console.log('in main.js')
     console.log(timesVisited)
 });
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
